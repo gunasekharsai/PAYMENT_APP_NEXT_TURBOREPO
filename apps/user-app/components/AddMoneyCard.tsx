ae4429a76dd55923a408b51e67363ea3cde3914b
GUNUPURU GUNASHEKAR SAI <113043690+gunasekharsai@users.noreply.github.com>
2024-04-19 20:02:24 +0530

    new

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
-import { TextInput } from "@repo/ui/textinput";
+import {TextInput} from "@repo/ui/textinput"
 import { createOnRampTransaction } from "../app/lib/actions/createOnrampTransaction";
 
 const SUPPORTED_BANKS = [{
@@ -42,7 +42,7 @@ export const AddMoney = () => {
             Add Money
             </Button>
 
-            
+
         </div>
     </div>
 </Card>
